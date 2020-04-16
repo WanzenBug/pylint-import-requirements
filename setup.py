bed1d1c9bad8998f746b1d339cd1261b242896bb
Fabian Peter Hammerle <fabian@hammerle.me>
2020-04-16 15:45:53 +0200

    package: added classifier indicating python3.4 support
    https://github.com/WanzenBug/pylint-import-requirements/pull/15

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
